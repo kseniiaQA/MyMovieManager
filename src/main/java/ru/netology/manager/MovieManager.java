7aa4092b33b79089b166c64852f53ab7b270f0d6
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-06-26 09:42:10 +0300

    Update MovieManager.java

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,60 +1,41 @@
 package ru.netology.manager;
 
 import ru.netology.domain.MovieItem;
+import ru.netology.repository.PosterRepository;
 
 public class MovieManager {
+    PosterRepository repository;
+    private MovieManager[] movies;
 
-
-    private int count = 10;
-    private MovieManager[] items = new MovieManager[0];
+    public MovieManager(PosterRepository repository) {
+        this.repository = this.repository;
+    }
 
 
     public MovieManager() {
 
     }
 
-    public MovieManager(int i2, int i1, String first, int i, int count) {
-        this.count = count;
-    }
 
-    public MovieManager(int i, String first, String film1, String detective) {
-    }
+    public void add(MovieItem movie) {
+        repository.save(movie);
+    } 
 
-
-    public void add(MovieManager item) {
-        // создаём новый массив размером на единицу больше
-        int length = items.length + 1;
-        MovieManager[] tmp = new MovieManager[length];
-        // itar + tab
-        // копируем поэлементно
-        // for (int i = 0; i < items.length; i++) {
-        //   tmp[i] = items[i];
-        // }
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        // кладём последним наш элемент
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
-    }
-
-    public MovieManager[] getAll() {
-
-        int newLength;
-        if (items.length < count) {
-            newLength = items.length;
-        } else {
-            newLength = count;
-        }
-        MovieManager[] result = new MovieManager[newLength];
-        // перебираем массив в прямом порядке
-        // но кладём в результаты в обратном
-        for (int i = 0; i < newLength; i++) {
-            result[i] = items[items.length - 1 - i];
+    public MovieItem[] getAll() {
+        MovieItem[] items = repository.findAll();
+        MovieItem[] result = new MovieItem[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
         }
         return result;
     }
-
-    public void getId() {
+ 
+    public void removeById(int id) {
+        repository.removeById(id);
     }
 
+    public MovieManager[] findAll() {
+        return movies;
+    }
 }
